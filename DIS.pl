eaef6683a5aa0c9aae01ee8f27e9efb64bff16fe
Prince of Archives <rich.tijerina@gmail.com>
2017-03-30 15:27:23 -0600

    Create DIS.pl

---
+++ DIS.pl
@@ -0,0 +1,36 @@
+use strict; use warnings;
+use Spreadsheet::ParseXLSX;
+#########################################
+# DIS - print values of spreadsheet range
+### DIS.pl sheet1 sheet2 row_1 col_1 row_2 col_2
+# SETUP #####################################
+my ($book, $start, $end, $row1, $col1, $row2, $col2) = @ARGV;
+die ("no book arg; book start end cell1 cell2 cell3") unless defined $book;
+die ("no start arg; book start end cell1 cell2 cell3") unless defined $start;
+die ("no end arg; book start end cell1 cell2 cell3") unless defined $end;
+die ("no cell arg; book start end cell1 cell2 cell3") unless defined $row1;
+open(my $bfh, '<', $book) or die ("FAIL OPEN $book\n");
+my $parser = Spreadsheet::ParseXLSX->new;
+my $wb = $parser->parse($book);
+die ("parser error") unless defined $wb;
+my @sheets;
+foreach ($wb->worksheets())
+    { push @sheets, $_->get_name(); }
+my $s = 0; my $e = 0;
+foreach (@sheets)
+    { last if $_ eq $start; $s++; }
+foreach (@sheets)
+    { last if $_ eq $end; $e++; }
+while ($s <= $e) {
+    my $ws = $wb->worksheet($s);
+    my $cell1 = $ws->get_cell($row1, $col1);
+    my $cell2 = $ws->get_cell($row2, $col2);
+    $s++;
+    my $sheet = $ws->get_name;
+    my $value1 = $cell1->value;
+    my $value2 = $cell2->value if defined $cell2;
+    print "$sheet : $value1";
+    if (defined $value2)
+        { print " : $value2"; }
+    print "\n";
+}
